efeca709e84cdde5710ce3fb6119f2ca3a9bf991
Cole Hajde <chajde@uccs.edu>
2024-04-16 13:19:53 -0600

    Selenium now integrated Successfully

--- emrem_app/tests.py
+++ emrem_app/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from selenium import webdriver
 
 # Create your tests here.
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+print(driver.title)  # Prints the title of the webpage opened
+driver.quit()
+
