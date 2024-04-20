69e2898c0e59b78144295f08c50cd2b1d0a21d4c
Cole Hajde <chajde@uccs.edu>
2024-04-20 10:48:37 -0600

    Emails now send!

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'PromptlyReminders@gmail.com'
-EMAIL_HOST_PASSWORD = 'ximu aqnf thri cuin'
+EMAIL_HOST_PASSWORD = 'ximuaqnfthricuin'
  
 
 # Application definition
--- emrem_app/management/commands/send_reminders.py
+++ emrem_app/management/commands/send_reminders.py
@@ -1,27 +1,38 @@
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from django.core.mail import send_mail
 from emrem_app.models import Reminder
-from django.conf import settings
+import smtplib
+from email.message import EmailMessage
 import ssl
+import certifi
 
 class Command(BaseCommand):
     help = 'Sends reminders that are due'
 
-    # Monkey-patch SSL to ignore certificate verification | VERY BAD PRACTICE, NEVER USE IN PRODUCTION
-    ssl._create_default_https_context = ssl._create_unverified_context
-    
-    
+    def send_email(self, subject, body, to_email):
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['Subject'] = subject
+        msg['From'] = 'PromptlyReminders@gmail.com'
+        msg['To'] = to_email
+
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+                
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls(context=ssl_context)
+            server.ehlo()
+            server.login('promtplyreminders@gmail.com', 'ximuaqnfthricuin')
+            server.send_message(msg)
+
     def handle(self, *args, **options):
         current_time = timezone.localtime()  # Ensure time is timezone aware
         start_of_day = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
         end_of_day = current_time.replace(hour=23, minute=59, second=59, microsecond=999999)
 
-        # Get reminders for today that haven't been sent yet
         reminders = Reminder.objects.filter(
             reminder_date__gte=start_of_day,
             reminder_date__lte=end_of_day,
-            sent=False  # Ensure that the reminder has not been marked as sent
+            sent=False
         )
 
         for reminder in reminders:
@@ -31,15 +42,12 @@ class Command(BaseCommand):
             elif reminder.urgency == 'SU':
                 subject = f"Important: {reminder.title}"
 
-
-            send_mail(
+            self.send_email(
                 subject,
                 reminder.description,
-                'PromptlyReminders@gmail.com',
-                [reminder.user.email],
-                fail_silently=False,
+                reminder.user.email
             )
-            reminder.sent = True  # Mark the reminder as sent
+            reminder.sent = True
             reminder.save()
 
             self.stdout.write(self.style.SUCCESS(f'Successfully sent reminder {reminder.id}'))
