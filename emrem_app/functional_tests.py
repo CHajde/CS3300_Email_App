b5b6bb4ee3489e7d0c7f2937e8ac1ef1a75915b3
Cole Hajde <chajde@uccs.edu>
2024-04-21 16:46:35 -0600

    Everything but Selenium working

--- emrem_app/functional_tests.py
+++ emrem_app/functional_tests.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Selenium testing --------------------------------------------
+
 class UserRegistrationTest(StaticLiveServerTestCase):
     def setUp(self):
         # Setup code to open the web browser
--- emrem_app/management/commands/send_reminders.py
+++ emrem_app/management/commands/send_reminders.py
@@ -16,12 +16,14 @@ class Command(BaseCommand):
         msg['From'] = 'PromptlyReminders@gmail.com'
         msg['To'] = to_email
 
+        # Use certifi to get the path to the CA bundle and create a SSL context
         ssl_context = ssl.create_default_context(cafile=certifi.where())
                 
+        # Send the email using Gmail's SMTP server and the provided credentials
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
             server.starttls(context=ssl_context)
             server.ehlo()
-            server.login('promtplyreminders@gmail.com', 'ximuaqnfthricuin')
+            server.login('promtplyreminders@gmail.com', 'ximuaqnfthricuin') # Unfortunate typo in the email address, cannot be fixed
             server.send_message(msg)
 
     def handle(self, *args, **options):
@@ -29,12 +31,14 @@ class Command(BaseCommand):
         start_of_day = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
         end_of_day = current_time.replace(hour=23, minute=59, second=59, microsecond=999999)
 
+        # Get reminders that are due today and have not been sent
         reminders = Reminder.objects.filter(
             reminder_date__gte=start_of_day,
             reminder_date__lte=end_of_day,
             sent=False
         )
 
+        # Custom email subject based on urgency
         for reminder in reminders:
             subject = f"Reminder: {reminder.title}"
             if reminder.urgency == 'VU':
@@ -42,12 +46,15 @@ class Command(BaseCommand):
             elif reminder.urgency == 'SU':
                 subject = f"Important: {reminder.title}"
 
+            # Call the send_email function which actually sends the email
             self.send_email(
                 subject,
                 reminder.description,
                 reminder.user.email
             )
+            # Set the reminder as 'sent' so it is not sent again
             reminder.sent = True
             reminder.save()
 
+            # Prints in the console that the reminder was sent, if successful
             self.stdout.write(self.style.SUCCESS(f'Successfully sent reminder {reminder.id}'))
