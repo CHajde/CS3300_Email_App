99e9dedc9ff513d4424e67db3364e8d06f199337
Cole Hajde <chajde@uccs.edu>
2024-04-21 18:57:16 -0600

    Improved formatting for email

--- emrem_app/management/commands/send_reminders.py
+++ emrem_app/management/commands/send_reminders.py
@@ -9,12 +9,21 @@ import certifi
 class Command(BaseCommand):
     help = 'Sends reminders that are due'
 
-    def send_email(self, subject, body, to_email):
+    def send_email(self, subject, body, to_email, reminder_time, username, urgency_label):
         msg = EmailMessage()
-        msg.set_content(body)
-        msg['Subject'] = subject
+        
+        # Setting the subject to include reminder title and time
+        msg['Subject'] = f"{subject} Today at {reminder_time.strftime('%H:%M')}"
         msg['From'] = 'PromptlyReminders@gmail.com'
         msg['To'] = to_email
+
+        # Formatting the body of the email
+        email_body = (f"Hello, {username}. You have a {urgency_label} reminder set for today.\n\n"
+                  f"Description of your Reminder:\n{body}\n\n"
+                  "This is an automated email, please do not respond.\n"
+                  "Thank you for using Promptly!")
+                     
+        msg.set_content(email_body)
 
         # Use certifi to get the path to the CA bundle and create a SSL context
         ssl_context = ssl.create_default_context(cafile=certifi.where())
@@ -30,6 +39,10 @@ class Command(BaseCommand):
         current_time = timezone.localtime()  # Ensure time is timezone aware
         start_of_day = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
         end_of_day = current_time.replace(hour=23, minute=59, second=59, microsecond=999999)
+        
+        print(f"Current Time: {current_time}")
+        print(f"Start of Day: {start_of_day}")
+        print(f"End of Day: {end_of_day}")
 
         # Get reminders that are due today and have not been sent
         reminders = Reminder.objects.filter(
@@ -37,24 +50,32 @@ class Command(BaseCommand):
             reminder_date__lte=end_of_day,
             sent=False
         )
+        
+        print(f"Found {reminders.count()} reminders due today.")
 
-        # Custom email subject based on urgency
-        for reminder in reminders:
-            subject = f"Reminder: {reminder.title}"
-            if reminder.urgency == 'VU':
-                subject = f"URGENT: {reminder.title}"
-            elif reminder.urgency == 'SU':
-                subject = f"Important: {reminder.title}"
+        if reminders.exists():
+            for reminder in reminders:
+                subject = f"Reminder: {reminder.title}"
+                if reminder.urgency == 'VU':
+                    subject = f"URGENT: {reminder.title}"
+                elif reminder.urgency == 'SU':
+                    subject = f"Important: {reminder.title}"
 
-            # Call the send_email function which actually sends the email
-            self.send_email(
-                subject,
-                reminder.description,
-                reminder.user.email
-            )
-            # Set the reminder as 'sent' so it is not sent again
-            reminder.sent = True
-            reminder.save()
+                # Call the send_email function which actually sends the email
+                self.send_email(
+                    subject,
+                    reminder.description,
+                    reminder.user.email,
+                    reminder.reminder_date,
+                    reminder.user.username,
+                    reminder.get_urgency_display()
+                )
+                # Set the reminder as 'sent' so it is not sent again
+                reminder.sent = True
+                reminder.save()
 
-            # Prints in the console that the reminder was sent, if successful
-            self.stdout.write(self.style.SUCCESS(f'Successfully sent reminder {reminder.id}'))
+                # Prints in the console that the reminder was sent, if successful
+                self.stdout.write(self.style.SUCCESS(f'Successfully sent reminder {reminder.id}'))
+        else:
+            # Print a message when no reminders are due to be sent
+            self.stdout.write(self.style.WARNING('No reminders are due to be sent at this time.'))
